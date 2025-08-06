e1c393a74b0275c006757a494208479a200a29cb
Artem Filippow <a.filippov@qtech.ru>
2025-08-06 11:42:25 +0900

    permissions

--- mailing/models.py
+++ mailing/models.py
@@ -14,6 +14,12 @@ class Recipient(models.Model):
 
     def __str__(self):
         return self.email
+    class Meta:
+        verbose_name = 'Recipient'
+        verbose_name_plural = 'Recipients'
+        permissions = [
+            ("can_view_recipient", "Can view recipient"),
+        ]
 
 
 class Message(models.Model):
@@ -25,6 +31,12 @@ class Message(models.Model):
 
     def __str__(self):
         return self.subject
+    class Meta:
+        verbose_name = 'Message'
+        verbose_name_plural = 'Messages'
+        permissions = [
+            ("can_view_message", "Can view messages"),
+        ]
 
 
 class Mailing(models.Model):
@@ -45,6 +57,13 @@ class Mailing(models.Model):
     def __str__(self):
         return f"{self.message.subject} {self.start} - {self.end} :: {self.status}"
 
+    class Meta:
+        verbose_name = 'Mailing'
+        verbose_name_plural = 'Mailings'
+        permissions = [
+            ("can_view_mailing", "Can view mailing"),
+        ]
+
 
 class Result(models.Model):
     """Result model"""
@@ -54,3 +73,9 @@ class Result(models.Model):
     email = models.CharField(max_length=50, verbose_name="Email", blank=True, null=True)
     response = models.TextField(max_length=50, verbose_name="ServerResponse", blank=True, null=True)
     mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE, related_name="Mailing")
+    class Meta:
+        verbose_name = 'Result'
+        verbose_name_plural = 'Results'
+        permissions = [
+            ("can_view_result", "Can view result"),
+        ]
